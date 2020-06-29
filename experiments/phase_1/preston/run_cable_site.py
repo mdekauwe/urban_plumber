d54bed991ddf5e792ffc01f391c7323bc2d9e631
mdekauwe <mdekauwe@gmail.com>
2020-06-29 11:41:12 +1000

    Added preston fixes

--- experiments/phase_1/preston/run_cable_site.py
+++ experiments/phase_1/preston/run_cable_site.py
@@ -25,6 +25,7 @@ from cable_utils import adjust_nml_file
 from cable_utils import get_svn_info
 from cable_utils import change_LAI, change_params
 from cable_utils import add_attributes_to_output_file
+from cable_utils import set_site_info
 
 class RunCable(object):
 
@@ -38,7 +39,7 @@ class RunCable(object):
                  cnpbiome_fname="pftlookup_csiro_v16_17tiles.csv",
                  elev_fname="GSWP3_gwmodel_parameters.nc",
                  lai_dir=None, fixed_lai=None, co2_conc=400.0,
-                 adjust_params=False, met_subset=[], cable_src=None,
+                 met_subset=[], cable_src=None,
                  cable_exe="cable", mpi=True, num_cores=None, verbose=True):
 
         self.met_dir = met_dir
@@ -66,17 +67,11 @@ class RunCable(object):
         self.num_cores = num_cores
         self.lai_dir = lai_dir
         self.fixed_lai = fixed_lai
-        self.adjust_params = adjust_params
-
-        if self.adjust_params:
-            self.soil_moisture_spinup = ".FALSE.",
-        else:
-            self.soil_moisture_spinup = ".TRUE.",
 
     def main(self, param_names=None, param_values=None, out_fname=None,
-             out_log_fname=None, mcmc_tag=None):
+             out_log_fname=None):
 
-        (met_files, url, rev) = self.initialise_stuff(mcmc_tag)
+        (met_files, url, rev) = self.initialise_stuff()
 
         # Setup multi-processor jobs
         if self.mpi:
@@ -96,7 +91,7 @@ class RunCable(object):
                 p = mp.Process(target=self.worker,
                                args=(met_files[start:end], url, rev,
                                      param_names, param_values,
-                                     out_fname, out_log_fname, mcmc_tag, ))
+                                     out_fname, out_log_fname, ))
                 processes.append(p)
 
             # Run processes
@@ -104,35 +99,29 @@ class RunCable(object):
                 p.start()
         else:
             self.worker(met_files, url, rev, param_names, param_values,
-                        out_fname, out_log_fname, mcmc_tag)
+                        out_fname, out_log_fname,)
 
     def worker(self, met_files, url, rev, param_names, param_values,
-               out_fname, out_log_fname, mcmc_tag):
+               out_fname, out_log_fname):
 
         for fname in met_files:
-            site = os.path.basename(fname).split(".")[0].split("_")[1]
+            site = os.path.basename(fname).split("_")[0]
 
             base_nml_fn = os.path.join(self.grid_dir, "%s" % (self.nml_fname))
 
-            if mcmc_tag is None:
-                nml_fname = "cable_%s.nml" % (site)
-            else:
-                nml_fname = "cable_%s_%s.nml" % (site, mcmc_tag)
+            nml_fname = "cable_%s.nml" % (site)
             shutil.copy(base_nml_fn, nml_fname)
 
-            # If MCMC these are passed.
-            if self.adjust_params == False:
-                (out_fname, out_log_fname) = self.clean_up_old_files(site)
+            (out_fname, out_log_fname) = self.clean_up_old_files(site)
 
             # Add LAI to met file?
             if self.fixed_lai is not None or self.lai_dir is not None:
                 fname = change_LAI(fname, site, fixed=self.fixed_lai,
                                    lai_dir=self.lai_dir)
 
-            # For MCMC
-            if self.adjust_params:
-                fname = change_params(fname, site, param_names, param_values,
-                                      mcmc_tag)
+
+            # Set tree, grass and bare fracs, measurement height etc
+            fname = set_site_info(fname, site)
 
             replace_dict = {
                             "filename%met": "'%s'" % (fname),
@@ -143,6 +132,8 @@ class RunCable(object):
                             "filename%veg": "'%s'" % (self.veg_fname),
                             "filename%soil": "'%s'" % (self.soil_fname),
                             "output%restart": ".FALSE.",
+                            #"output%patch": ".TRUE.", # get all patches
+                            "output%patch": ".FALSE.", # get all patches
                             "fixedCO2": "%.2f" % (self.co2_conc),
                             "casafile%phen": "'%s'" % (self.phen_fname),
                             "casafile%cnpbiome": "'%s'" % (self.cnpbiome_fname),
@@ -150,7 +141,7 @@ class RunCable(object):
                             "cable_user%GS_SWITCH": "'medlyn'",
                             "cable_user%GW_MODEL": ".FALSE.",
                             "cable_user%or_evap": ".FALSE.",
-                            "spinup":"%s" % (self.soil_moisture_spinup),
+                            "spinup": ".FALSE.",
                             "verbose": ".FALSE.",
             }
             adjust_nml_file(nml_fname, replace_dict)
@@ -163,14 +154,7 @@ class RunCable(object):
             if self.fixed_lai is not None or self.lai_dir is not None:
                 os.remove("%s_tmp.nc" % (site))
 
-            if self.adjust_params:
-                if mcmc_tag is not None:
-                    os.remove("%s_%s_tmp.nc" % (site, mcmc_tag))
-                else:
-                    os.remove("%s_tmp.nc" % (site))
-                #os.remove(nml_fname)
-
-    def initialise_stuff(self, mcmc_tag):
+    def initialise_stuff(self):
 
         if not os.path.exists(self.restart_dir):
             os.makedirs(self.restart_dir)
@@ -191,22 +175,14 @@ class RunCable(object):
             met_files = [os.path.join(self.met_dir, i) for i in self.met_subset]
 
         cwd = os.getcwd()
-        (url, rev) = get_svn_info(cwd, os.path.join(self.cable_src, "offline"),
-                                  mcmc_tag)
+        (url, rev) = get_svn_info(cwd, os.path.join(self.cable_src, "offline"))
 
-        if self.adjust_params == False:
-            # delete local executable, copy a local copy and use that
-            local_exe = "cable"
-            if os.path.isfile(local_exe):
-                os.remove(local_exe)
-            shutil.copy(self.cable_exe, local_exe)
-            self.cable_exe = local_exe
-        else:
-            # Otherwise multi-core runs may clean up cable exe when we need it
-            local_exe = "./cable"
-            if not os.path.exists(local_exe):
-                shutil.copy(self.cable_exe, local_exe)
-            self.cable_exe = local_exe
+        # delete local executable, copy a local copy and use that
+        local_exe = "cable"
+        if os.path.isfile(local_exe):
+            os.remove(local_exe)
+        shutil.copy(self.cable_exe, local_exe)
+        self.cable_exe = local_exe
 
         return (met_files, url, rev)
 
@@ -239,22 +215,21 @@ class RunCable(object):
 if __name__ == "__main__":
 
     #------------- Change stuff ------------- #
-    met_dir = "met/raw"
+    met_dir = "met"
     log_dir = "logs"
     output_dir = "outputs"
     restart_dir = "restarts"
     namelist_dir = "namelists"
-    aux_dir = "../../../src/CABLE-AUX/"
-    cable_src = "../../../src/trunk/trunk"
+    aux_dir = "../../../src/trunk_plumber/CABLE-AUX/"
+    cable_src = "../../../src/trunk_plumber/trunk_plumber"
     mpi = False
     num_cores = 1 # set to a number, if None it will use all cores...!
     # if empty...run all the files in the met_dir
-    met_subset = ['01_preston_metforcing_1993_2004_UTC_v1.nc']
+    met_subset = ['AU-Preston_metforcing_v1.nc']
     #------------------ ----------------------#
 
     C = RunCable(met_dir=met_dir, log_dir=log_dir, output_dir=output_dir,
                  restart_dir=restart_dir, aux_dir=aux_dir,
                  namelist_dir=namelist_dir, met_subset=met_subset,
-                 cable_src=cable_src, mpi=mpi, num_cores=num_cores,
-                 adjust_params=False)
+                 cable_src=cable_src, mpi=mpi, num_cores=num_cores)
     C.main()
