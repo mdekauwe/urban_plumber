2270d244177109b0622a2eafac99c3bebc9b096c
mdekauwe <mdekauw@gmail.com>
2021-02-02 09:10:13 +1100

    fixed warning

--- experiments/phase_1/AU-Preston/run_cable_site.py
+++ experiments/phase_1/AU-Preston/run_cable_site.py
@@ -202,14 +202,14 @@ class RunCable(object):
         if self.verbose:
             cmd = './%s %s' % (self.cable_exe, nml_fname)
             error = subprocess.call(cmd, shell=True)
-            if error is 1:
+            if error == 1:
                 print("Job failed to submit")
                 raise
         else:
             # No outputs to the screen: stout and stderr to dev/null
             cmd = './%s %s > /dev/null 2>&1' % (self.cable_exe, nml_fname)
             error = subprocess.call(cmd, shell=True)
-            if error is 1:
+            if error == 1:
                 print("Job failed to submit")
 
 if __name__ == "__main__":
